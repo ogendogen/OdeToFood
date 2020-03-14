20219407ee964a6f958ec18cdebff0343c048d3f
ogendogen <kosmos500@interia.pl>
2020-03-14 23:05:33 +0100

    Following the POST-REDIRECT-GET pattern

--- OdeToFood/Pages/Restaurants/Edit.cshtml.cs
+++ OdeToFood/Pages/Restaurants/Edit.cshtml.cs
@@ -41,6 +41,7 @@ namespace OdeToFood.Pages.Restaurants
             {
                 restaurantData.Update(Restaurant);
                 restaurantData.Commit();
+                return RedirectToPage("./Detail", new { restaurantId = Restaurant.Id});
             }
 
             Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
