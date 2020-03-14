b97dd2925d8a164c44e10b163f1b1d928dff16a1
ogendogen <kosmos500@interia.pl>
2020-03-14 17:29:09 +0100

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+    }
+}
---
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private readonly List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant() { Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
+                new Restaurant() { Id = 2, Name = "Mr. Hamburger", Location = "Strzemki", Cuisine = CuisineType.None},
+                new Restaurant() { Id = 3, Name = "Papa Diego", Location = "Katowice", Cuisine = CuisineType.Mexican}
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return restaurants.OrderBy(r => r.Name).Select(r => r);
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return GetAll();
+            }
+
+            return restaurants.OrderBy(r => r.Name)
+                .Where(r => r.Name.StartsWith(name))
+                .Select(r => r);
+        }
+    }
+}
